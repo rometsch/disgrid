b5ab7290553835910b8273f14f613728d14185e4
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-05-09 19:59:45 +0200

    a script to run all unit tests

---
+++ unit_test_suite.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import unittest
+
+from tests.test_data import *
+from tests.test_field import *
+from tests.test_fluid import *
+from tests.test_grid import *
+
+if __name__=="__main__":
+    unittest.main()
