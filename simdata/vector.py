eb354b0fa6f378ac99ae0c95c464ef63daea6ab5
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-05-17 15:50:44 +0200

    add vector class for time series of scalar and vector quantities

---
+++ simdata/vector.py
@@ -0,0 +1,57 @@
+# vector quantities and time series
+# data is stored as Ntimes x Ndata
+import numpy as np
+import astropy.units as u
+
+class Vector:
+
+    def __init__(self, time, data, axes=[], name=""):
+        self.time = time
+        self.data = data
+        self.name = name
+        self.axes = axes
+        for v in [time, data]:
+            if not isinstance(v, u.Quantity):
+                raise TypeError("'{}' is a physical value but not an astropy quantity!".format(v))
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def get(self, key=None, low=None, up=None, axis=None, return_time=False):
+        # TODO: handle keys
+        # handle
+        # 1) array of indices [1, 5, 7 ,8]
+        # 3) range operator range(2,20)
+        if key is None:
+            if low is None and up is None:
+                raise ValueError("No key and neither upper nor lower bound given!")
+            if low is not None and up is not None:
+                inds = range(low,up)
+            elif low is None:
+                inds = range(0, up)
+            elif up is None:
+                inds = range(low, len(self.data))
+        elif type(key) in [range, slice]:
+            inds = key
+        else:
+            try:
+                len(key)
+                inds = key
+            except TypeError:
+                raise TypeError("Key is neither a range nor a list of indices")
+        rv = self.data[inds]
+        if axis is not None:
+            if axis in axes:
+                n_axis = axes[axis]
+                rv = rv[:, n_axis]
+        if return_time:
+            rv = (self.time[inds], rv)
+        return rv
+
+    def get_time_interval(self, tmin, tmax, **kwargs):
+        for t in [tmin, tmax]:
+            if not isinstance(t, u.Quantity):
+                raise TypeError("'{}' does not have a unit!".format(t))
+        inds = np.logical_and( tmin <= self.time, tmax >= self.time)
+        key = np.arange(len(inds))[inds]
+        return self.get(key, **kwargs)
---
+++ tests/test_vector.py
@@ -0,0 +1,48 @@
+import unittest
+import astropy.units as u
+import numpy as np
+from simdata import vector
+
+class TestDataMethods(unittest.TestCase):
+    def setUp(self):
+        self.t = 0.5*np.arange(10)*u.s
+        np.random.seed(0)
+        self.d = np.random.rand(10)*u.cm
+        name = 'distance'
+        axes = ['x']
+        self.v = vector.Vector(self.t, self.d, name=name, axes=axes)
+
+    def test_no_unit(self):
+        with self.assertRaises(TypeError):
+            vector.Vector([0,1], [1,2]*u.s)
+        with self.assertRaises(TypeError):
+            vector.Vector([0,1]*u.s, [1,2])
+
+    def test_get_ind_list(self):
+        self.assertTrue( all( self.v.get([2,4,7]) == self.d[[2,4,7]]))
+    def test_get_low_up(self):
+        self.assertTrue( all( self.v.get(low=2, up=7) == self.d[2:7]))
+        self.assertTrue( all( self.v.get(up=7) == self.d[:7]))
+        self.assertTrue( all( self.v.get(low=7) == self.d[7:]))
+        with self.assertRaises(ValueError):
+            self.v.get()
+
+    def test_get_range(self):
+        self.assertTrue( all( self.v.get(range(2,8)) == self.d[2:8]))
+
+    def test_get_slice(self):
+        self.assertTrue( all( self.v.get(slice(2,8,2)) == self.d[2:8:2]))
+        self.assertTrue( all( self.v[2:8:2] == self.d[2:8:2]))
+
+    def test_get_time(self):
+        t, v = self.v.get(range(2,8), return_time=True)
+        self.assertTrue( all( t == self.t[2:8]))
+
+    def test_get_time_interval(self):
+        t, v = self.v.get_time_interval(0.75*u.s, 3.9*u.s, return_time=True)
+        self.assertTrue( all( t == self.t[2:8]))
+        with self.assertRaises(TypeError):
+            self.v.get_time_interval(0.75, 3.9*u.s, return_time=True)
+
+if __name__ == '__main__':
+    unittest.main()
