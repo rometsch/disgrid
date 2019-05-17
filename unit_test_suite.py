a1a9b5887a9efec27ccf48450699ed7dc1820c74
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-05-17 15:51:24 +0200

    add vector test to test suite

--- unit_test_suite.py
+++ unit_test_suite.py
@@ -5,6 +5,7 @@ from tests.test_data import *
 from tests.test_field import *
 from tests.test_fluid import *
 from tests.test_grid import *
+from tests.test_vector import *
 
 if __name__=="__main__":
     unittest.main()
