ccf11e23a4c30ceabff3e6f31faac6564f48090a
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-05-17 14:16:59 +0200

    update loader strcuture

--- simdata/loaders/interface.py
+++ simdata/loaders/interface.py
@@ -1,13 +1,24 @@
-class LoaderInterface:
-
-    def __init__(self):
-        pass
+# a loader object orchestrates the loading of data from files
+# it 
+# it provides a framework for caching data and provides function which return field objects
+class Loader:
+    def __init__(self, path):
+        self.path = path
+        self.
 
 class Quantity:
-
     def __init__(self, loader):
         self.loader = loader
         self.dataDir = loader.dataDir
 
-class FieldQuantity(Quantity):
+class Field3d(Quantity):
+    pass
+
+class Field2d(Quantity):
+    pass
+        
+class Field1d(Quantity):
+    pass
+
+class Scalar(Quantity):
     pass
