275cd7941c7ba871d3d0cc86ee11d30d1a40d703
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-10-08 17:32:32 +0200

    allow search_args to be parsed to simscripts lookup

--- simdata/data.py
+++ simdata/data.py
@@ -8,8 +8,8 @@ import copy
 from . import loaders
 
 class Data:
-    def __init__(self, path=os.getcwd(), data_dir=None, loader=None, **kwargs):
-        self.path = try_simscripts_lookup(path)
+    def __init__(self, path=os.getcwd(), data_dir=None, loader=None, search_args=None, **kwargs):
+        self.path = try_simscripts_lookup(path, search_args)
         self.code, self.loader = loaders.get_loader(self.path, loader, **kwargs)
         if data_dir is not None:
             self.loader.set_data_dir(data_dir)
@@ -23,14 +23,19 @@ class Data:
     def get_fluid(self, name):
         return self.fluids[name]
 
-def try_simscripts_lookup(pattern):
+def try_simscripts_lookup(pattern, search_args=None):
     try:
         import simscripts.cache
         c = simscripts.cache.Cache()
         try:
-            rv = c.search(pattern)["path"]
+            if search_args is not None:
+                rv = c.search(pattern, **search_args)["path"]
+            else:
+                rv = c.search(pattern)["path"]
         except simscripts.cache.NoSimulationFoundError:
             rv = pattern
+        except simscripts.cache.ResultNotUniqueError:
+            raise
     except ImportError:
         rv = pattern
     return rv
