87de78cfa17635d352380b9071347e53b6850da0
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-10-09 15:58:11 +0200

    version string back to 0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_namespace_packages
 
 setup(  name="simdata"
-        ,version="0.1b1"
+        ,version="0.1"
         ,description="scripts to load hydro simulation output"
         ,author="Thomas Rometsch, Peter Rodenkirch"
         ,package_dir={'': 'src'}
