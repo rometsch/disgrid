0b4c9750ad85249c1cddeb2caddbf3bd97e155e4
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-10-11 01:25:18 +0200

    support remote simulation data via simscripts and sshfs

--- src/simdata/data.py
+++ src/simdata/data.py
@@ -8,11 +8,8 @@ import copy
 from . import loaders
 
 class Data:
-    def __init__(self, path=os.getcwd(), data_dir=None, loader=None, search_args=None, **kwargs):
-        self.path = try_simscripts_lookup(path, search_args)
-        if not os.path.exists(self.path):
-            raise FileNotFoundError("This path does not exist: '{}', abspath: '{}'".format(self.path, os.path.abspath(self.path)))
-        self.code, self.loader = loaders.get_loader(self.path, loader, **kwargs)
+    def __init__(self, path=os.getcwd(), data_dir=None, loader=None, **kwargs):
+        self.code, self.loader = loaders.get_loader(path, loader, **kwargs)
         if data_dir is not None:
             self.loader.set_data_dir(data_dir)
         self.loader.scout()
@@ -25,22 +22,57 @@ class Data:
     def get_fluid(self, name):
         return self.fluids[name]
 
-def try_simscripts_lookup(pattern, search_args=None):
-    rv = pattern
+def simscripts_global_lookup(pattern, remote=True, search_args=None):
     try:
-        import simscripts.cache
-        c = simscripts.cache.Cache()
+        from simscripts.search import search
         try:
-            if search_args is not None:
-                rv = c.search(pattern, **search_args)["path"]
-            else:
-                rv = c.search(pattern)["path"]
-        except simscripts.cache.NoSimulationFoundError:
-            pass
-        except simscripts.cache.ResultNotUniqueError:
-            print("Simscripts result is not unique!")
+            if search_args is None:
+                search_args = {}
+            rv = search(pattern, remote=remote, **search_args)
+            # use the first match
+            if len(rv) == 0:
+                raise KeyError("Could not locate simulation for pattern : '{}'".format(pattern))
+            return rv[0]
+
         except Exception as e:
             print("Simscripts lookup failed with exception: {}".format(e))
+            raise
     except ImportError:
-        pass
-    return rv
+        print("Simscripts is not installed on this maschine!")
+        return {}
+
+def mount_sshfs(remote, local):
+    # mount a remote location to a local directory using sshfs
+    from subprocess import run
+    run(["sshfs", "-o", "ro", remote, local])
+
+def unmount_sshfs(local):
+    # unmount a sshfs mount
+    from subprocess import run
+    run(["fusermount", "-u", local])
+
+class SData(Data):
+    # data loader with simscripts support to locate remote simulations
+    # and mount them via sshfs
+    def __init__(self, simid, remote=True, search_args=None, **kwargs):
+        self.sim = simscripts_global_lookup(simid, remote=remote, search_args=search_args)
+        self.is_remote = self.sim["host"] != "localhost"
+        if self.is_remote:
+            self.path = self.mount()
+        else:
+            self.path = self.sim["path"]
+        super().__init__(self.path, **kwargs)
+
+    def mount(self):
+        from simscripts.search import remote_path
+        from tempfile import TemporaryDirectory
+        rpath = remote_path(self.sim)
+        self.tempdir = TemporaryDirectory(suffix=self.sim["uuid"], prefix="simdata-")
+        lpath = self.tempdir.name
+        mount_sshfs(rpath, lpath)
+        return lpath
+
+    def __del__(self):
+        unmount_sshfs(self.path)
+        import time
+        time.sleep(0.5)
