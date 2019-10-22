8ed083fc1cec1c7825ad378cec0a32d440b2bc81
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-10-22 08:05:36 +0200

    support local data through RemoteData

--- src/simdata/remotedata.py
+++ src/simdata/remotedata.py
@@ -8,10 +8,17 @@ class RemoteData(Data):
     Use user@host:path as the remote_path argument. The syntax of the remote path is equivalent to the arguments of scp."""
     def __init__(self, remote_path, **kwargs):
         self.remote_path = remote_path
-        self.path = self.mount()
+        if is_local_path(self.remote_path):
+            self.path = self.remote_path
+        else:
+            self.path = self.mount()
         super().__init__(self.path, **kwargs)
 
     def mount(self):
-        self.mount = Mount(self.remote_path)
-        local_path = self.mount.get_path()
+        self.mount_point = Mount(self.remote_path)
+        local_path = self.mount_point.get_path()
         return local_path
+
+def is_local_path(path):
+    """ Evaluate whether 'path' is not of the form host:path. """
+    return len(path.split(":")) < 2
