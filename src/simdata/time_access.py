064a2b3e9aaffe9db180ac252ffe6639c19876a4
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-11-13 10:30:01 +0100

    when getting value by time, make sure its inside the time interval

--- src/simdata/time_access.py
+++ src/simdata/time_access.py
@@ -44,6 +44,7 @@ def get_index_closest_time(t, time):
     """
     ensure_unit(time)
     ensure_unit(t)
+    ensure_in_time_interval(t, time)
     ind = np.argmin(np.abs(time - t))
     return ind
 
@@ -52,3 +53,30 @@ def ensure_unit(x):
     """ Ensure that x is an astropy quantity. Raise an error otherwise. """
     if not isinstance(x, u.Quantity):
         raise TypeError("'{}' does not have a unit!".format(x))
+
+def ensure_in_time_interval(t, time, delta=0.001):
+    """ Ensure that t is in the time interval of time.
+
+    Raise an error if time is not within the interval defined by
+    time +- some tolerance defined by delta.
+
+    Parameters
+    ----------
+    t : :obj:`astropy.units.quantity.Quantity`
+        Time to be checked.
+    time : :obj:`astropy.units.quantity.Quantity`
+        Time array to define the allowed interval.
+    delta : float
+        Percentage of the time interval to add as tolerance.
+
+    Raises
+    ------
+    ValueError
+        If t is not inside time interval + tolerance.
+    """
+    tmin = time[0] - delta*(time[-1] - time[0])
+    tmax = time[-1] + delta*(time[-1] - time[0])
+    if t > tmax or t < tmin:
+        raise ValueError("t={} is not inside time interval [{}, {}]"
+                         .format(t, tmin, tmax), ", delta = {}".format(delta))
+
