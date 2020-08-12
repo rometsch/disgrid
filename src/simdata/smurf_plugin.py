4783272a278e436121f5b02fb5d02fb9f3f5aeba
Thomas Rometsch <thomas.rometsch@gmail.com>
2020-08-12 10:25:54 +0200

    fix autoadding dirs

--- src/simdata/smurf_plugin.py
+++ src/simdata/smurf_plugin.py
@@ -4,16 +4,20 @@
 # to transparently get a data object without manually
 # looking up simulation dir paths.
 
-from smurf.search import remote_path, search
-from smurf.cache import get_cache_by_id
-from smurf.mount import Mount
+import os
+
 import simdata.data
+from smurf.cache import LocalSimCache, get_cache_by_id, CacheMiss
+from smurf.info import Info
+from smurf.mount import Mount
+from smurf.search import remote_path, search
 
 
 class RemoteData(simdata.data.Data):
     """ Data interface for simulations on remote hosts.
     Simdirs on remote hosts are mounted using sshfs. 
     Use user@host:path as the remote_path argument. The syntax of the remote path is equivalent to the arguments of scp."""
+
     def __init__(self, remote_path, cache_timeout=None, **kwargs):
         self.remote_path = remote_path
         if is_local_path(self.remote_path):
@@ -32,6 +36,7 @@ class SmurfData(RemoteData):
     # data loader with smurf support to locate remote simulations
     # and mount them via sshfs
     def __init__(self, simid, search_remote=True, search_args={}, **kwargs):
+        simid = insert_local_sim_to_cache(simid)
         self.sim = search(simid,
                           remote=search_remote,
                           unique=True,
@@ -47,6 +52,24 @@ class SmurfData(RemoteData):
         c = get_cache_by_id(self.sim["uuid"])
         c.insert(self.sim["uuid"], self.sim)
 
+
+def insert_local_sim_to_cache(path):
+    """ Try to insert a local directory to the cache if not present. """
+    abspath = os.path.abspath(path)
+    if os.path.isdir(os.path.abspath(path)):
+        try:
+            info = Info(abspath)
+            try:
+                lc = LocalSimCache()
+                lc.request(info.uuid)
+                rv = info.uuid
+            except CacheMiss:
+                rv = lc.add_sim_to_cache(abspath)
+        except FileNotFoundError:
+            rv = path
+    else:
+        rv = path
+    return rv
 
 def is_local_path(path):
     """ Evaluate whether 'path' is not of the form host:path. """
