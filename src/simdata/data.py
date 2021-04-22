3a2485b5cb76256df7d37caed3b2c89e171e5ff4
Thomas Rometsch <thomas.rometsch@gmail.com>
2021-04-22 14:59:42 +0200

    only generate loaders at first access

--- src/simdata/data.py
+++ src/simdata/data.py
@@ -10,15 +10,38 @@ from . import loaders
 
 class Data:
     """ Create a data interface for the data in 'path' """
-    def __init__(self, path=os.getcwd(), loader=None, **kwargs):
-        self.code, self.loader = loaders.get_loader(path, loader, **kwargs)
+
+    def __init__(self, path=os.getcwd(), loader=None, init_hooks=None, **kwargs):
+        self.path = path
+        self.loader = loader
+        self.kwargs = kwargs
+        self.init_hooks = init_hooks
+
+    def init(self):
+        self.code, self.loader = loaders.get_loader(
+            self.path, self.loader, **self.kwargs)
         self.loader.scout()
         self.fluids = self.loader.fluids
         self.particles = self.loader.particles
-        self.particlegroups = {"particles" : self.particles}
+        self.particlegroups = {"particles": self.particles}
         self.planets = self.loader.planets
         self.parameters = self.loader.parameters
         self.register_postprocessor()
+        if self.init_hooks is not None:
+            for func in self.init_hooks:
+                func()
+
+    def __getattr__(self, attr):
+        try:
+            return self.__dict__[attr]
+        except KeyError:
+            if attr in ["path", "code", "loader", "fluids",
+                        "particles", "particlegroups",
+                        "planets", "paramters"]:
+                self.init()
+                return self.__dict__[attr]
+            else:
+                raise AttributeError(f'{attr}')
 
     def get_fluid(self, name):
         return self.fluids[name]
