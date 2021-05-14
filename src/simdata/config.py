30404e71a03aa1d7a76b70d74e88d953ae68e5bd
Thomas Rometsch <thomas.rometsch@gmail.com>
2021-05-14 10:13:09 +0200

    add config module

---
+++ src/simdata/config.py
@@ -0,0 +1,34 @@
+import os
+import json
+
+home_path = os.path.join(os.path.expanduser("~"), ".simdata")
+
+
+class Config:
+    def __init__(self):
+        self.config_file = os.path.join(home_path, "config.json")
+        if not os.path.exists(home_path):
+            self.create()
+        else:
+            self.load()
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def create(self):
+        os.makedirs(home_path)
+        self.load()
+        self.save()
+
+    def save(self):
+        self.data["type"] = "simdata config"
+        self.data["version"] = "0.1"
+        with open(self.config_file, "w") as out_file:
+            out_file.write(json.dumps(self.data, indent=4))
+
+    def load(self):
+        try:
+            with open(self.config_file, "r") as in_file:
+                self.data = json.load(in_file)
+        except FileNotFoundError:
+            self.data = {}
