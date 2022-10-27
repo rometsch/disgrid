2c41f49e894f043f2c7e2df4e4cb713aa77f4572
Thomas Rometsch <thomas.rometsch@gmail.com>
2022-10-27 20:02:58 +0200

    catch correct exception in import try

--- src/disgrid/__init__.py
+++ src/disgrid/__init__.py
@@ -3,7 +3,7 @@ from .data import Data
 try:
     from .smurf_plugin import RemoteData
     from .smurf_plugin import SmurfData as SData
-except ImportError:
+except (ModuleNotFoundError,ImportError):
     pass
 
 try:
